515195e08cf66cc6e5877cb890d3d896f781b877
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2025-12-26 21:11:41 +0500

    Removed Main.storyboard

--- Swift Concurrency/Swift Concurrency/ViewController.swift
+++ Swift Concurrency/Swift Concurrency/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Swift Concurrency
-//
-//  Created by Anton Vikhlyaev on 26.12.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
     }
-
-
 }
-
