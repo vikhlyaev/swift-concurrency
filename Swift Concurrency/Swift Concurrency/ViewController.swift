e2e19ade921491c5dedb1f51ade2ff7f0a8cb08b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2025-12-26 21:54:14 +0500

    Added file structure

--- Swift Concurrency/Swift Concurrency/ViewController.swift
+++ Swift Concurrency/Swift Concurrency/ViewController.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
     }
 }
